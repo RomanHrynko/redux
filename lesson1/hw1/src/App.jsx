3f026c61512e40ef6525a4c2e1d89a943656f8d5
Roman Hrynko <romanhrynkoua@gmail.com>
2021-07-18 19:54:12 +0300

    lesson1-hw1

---
+++ lesson1/hw1/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Header from './Header.jsx';
+import { UserContext } from './user-context.js';
+
+class App extends React.Component {
+  state = {
+    userData: {
+      name: 'Nikola Tesla',
+      avatar_url: 'https://avatars3.githubusercontent.com/u10001',
+    },
+  };
+  render() {
+    return (
+      <div className="page">
+        <UserContext.Provider value={this.state.userData}>
+          <Header />
+        </UserContext.Provider>
+      </div>
+    );
+  }
+}
+
+export default App;

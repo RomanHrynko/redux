2d615d56f5f348e0c3254b67d66b2710be43e12b
Roman Hrynko <romanhrynkoua@gmail.com>
2021-07-20 15:55:42 +0300

    lesson5-task1-v2

--- lesson5/task1/src/users/UsersList.jsx
+++ lesson5/task1/src/users/UsersList.jsx
@@ -5,40 +5,30 @@ import Pagination from './Pagination.jsx';
 import User from './User.jsx';
 import { usersListSelectors, currentPageSelectors } from './users.selectors';
 
-class UsersList extends React.Component {
-  goPrev = () => {
-    this.props.prevPage();
-  };
+const UsersList = ({ users, prevPage, nextPage }) => {
+  const { currentPage, usersList } = users;
+  const usersPerPage = 3;
 
-  goNext = () => {
-    this.props.nextPage();
-  };
+  const start = currentPage * usersPerPage;
+  const usersToDisplay = usersList.slice(start, start + usersPerPage);
 
-  render() {
-    const { currentPage, usersList } = this.props.users;
-    const usersPerPage = 3;
-
-    const start = currentPage * usersPerPage;
-    const usersToDisplay = usersList.slice(start, start + usersPerPage);
-
-    return (
-      <div>
-        <Pagination
-          goPrev={this.goPrev}
-          goNext={this.goNext}
-          currentPage={currentPage}
-          totalItems={usersList.length}
-          itemsPerPage={usersPerPage}
-        />
-        <ul className="users">
-          {usersToDisplay.map(user => (
-            <User key={user.id} {...user} />
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Pagination
+        goPrev={prevPage}
+        goNext={nextPage}
+        currentPage={currentPage}
+        totalItems={usersList.length}
+        itemsPerPage={usersPerPage}
+      />
+      <ul className="users">
+        {usersToDisplay.map(user => (
+          <User key={user.id} {...user} />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 const mapState = state => {
   return {

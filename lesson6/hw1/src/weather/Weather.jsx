a21aea63d96227f782cd9adfb5040e7507b9fb78
Roman Hrynko <romanhrynkoua@gmail.com>
2021-07-21 16:14:18 +0300

    lesson6-hw1

---
+++ lesson6/hw1/src/weather/Weather.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { weatherDataSelector } from './weather.selectors';
+import * as usersActions from './weather.actions';
+
+const Weather = ({ weatherData, getWeatherData }) => {
+  useEffect(() => {
+    getWeatherData();
+  }, []);
+
+  return (
+    <main className="weather">
+      <h1 className="weather__title">Weather data</h1>
+      <ul className="cities-list">
+        {weatherData.map(weather => (
+          <li className="city">
+            <span className="city__name">{weather.name}</span>
+            <span className="city__temperature">{`${weather.temperature} F`}</span>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+};
+
+const mapState = state => {
+  return {
+    weatherData: weatherDataSelector(state),
+  };
+};
+
+const mapDispatch = {
+  getWeatherData: usersActions.getWeatherData,
+};
+
+export default connect(mapState, mapDispatch)(Weather);
---
+++ lesson6/hw1/src/weather/weather.actions.js
@@ -0,0 +1,20 @@
+import { fetchWeatherData } from './weather.gateway';
+
+export const ADD_WEATHER_DATA = 'ADD_WEATHER_DATA';
+
+export const weatherDataReceived = weatherData => {
+  return {
+    type: ADD_WEATHER_DATA,
+    payload: {
+      weatherData,
+    },
+  };
+};
+
+export const getWeatherData = () => {
+  return function (dispatch) {
+    fetchWeatherData().then(weatherData => {
+      dispatch(weatherDataReceived(weatherData));
+    });
+  };
+};
